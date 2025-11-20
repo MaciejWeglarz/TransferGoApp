60a5ab75af9dafc94fb63bbbf52a1c1da3478280
MWeglarz <maciejweglarz97@gmail.com>
2025-11-20 17:50:59 +0100

    added strings, colors, removed unused imports, deleted comments

--- app/src/main/java/com/maciejweglarz/transfergoapp/ui/theme/Color.kt
+++ app/src/main/java/com/maciejweglarz/transfergoapp/ui/theme/Color.kt
@@ -1,3 +1,4 @@
+// Color.kt
 package com.maciejweglarz.transfergoapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
@@ -9,3 +10,19 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Converter
+val ConverterErrorBorder = Color(0xFFFF4F9A)
+val ConverterCardBackground = Color(0xFFF1F4F8)
+val ConverterSendingAmount = Color(0xFF0084FF)
+val ConverterReceiverBackground = Color(0xFFEDF0F4)
+val ConverterSubtitleText = Color(0xFF8C9199)
+val ConverterScreenBackground = Color(0xFFE5E5E5)
+
+val ConverterRateChipBackground = Color(0xFF000000)
+val ConverterRateChipText = Color(0xFFFFFFFF)
+
+val ConverterNetworkBannerBackground = Color(0xFFFFE5E5)
+val ConverterNetworkBannerText = Color(0xFFB00020)
+val ConverterCurrencyListDivider = Color(0xFFE4E7EB)
+val ConverterErrorBackground = Color(0xFFFFF0F4)
